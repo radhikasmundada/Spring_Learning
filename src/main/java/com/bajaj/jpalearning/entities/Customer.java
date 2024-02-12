97873367d7e62676b2290477f5ff674b64eabd97
Radhika Mundada <radhikamundada4848@gmail.com>
2024-02-12 10:58:08 +0530

    Added constructor and getter setters

--- src/main/java/com/bajaj/jpalearning/entities/Customer.java
+++ src/main/java/com/bajaj/jpalearning/entities/Customer.java
@@ -14,8 +14,55 @@ public class Customer {
 
     private int age;
 
+    @Column(unique = true)
     private String emailId; // Write in camel case it will take email_ID
 
+    private String password;
 
+    public Customer(String name, int age, String emailId, String password) {
+        this.name = name;
+        this.age = age;
+        this.emailId = emailId;
+        this.password = password;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
